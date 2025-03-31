c99ffde82feac52057430da73f71b4310bb89caf
KasumiiUwU <p.khoa62@gmail.com>
2025-03-31 09:36:08 +0700

    remove

--- src/apis/ApiEndpoint.jsx
+++
@@ -1,9 +0,0 @@
-const API_BASE_URL = "https://api.yourwebsite.com";
-
-export const API_ENDPOINTS = {
-  BASE_URL: '${API_BASE_URL}',
-  LOGIN: `${API_BASE_URL}/auth/login`,
-  REGISTER: `${API_BASE_URL}/auth/register`,
-  USER_PROFILE: `${API_BASE_URL}/user/profile`,
-  PRODUCTS: `${API_BASE_URL}/products`,
-};
