26c583c5db072d8301f40aac06b34fb08dd81ae3
KasumiiUwU <p.khoa62@gmail.com>
2025-04-05 13:41:00 +0700

    add login function

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/login/Login";
 import Home from "./pages/home/Home";
-
+import CarDetail from "./pages/carDetail/CarDetail";
+import FindCar from "./pages/findCar/FindCar";
 
 
 function App() {
@@ -12,7 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-
+        <Route path="/carDetail" element={<CarDetail />} />
+        <Route path="/findcar" element={<FindCar />} />
       </Routes>
     </Router>
   );
---
+++ src/services/authService.ts
@@ -0,0 +1,32 @@
+import axiosClient from "../utils/axiosClient";
+
+const AUTH_API_URL = '/authentication-api/api/Authentication';
+
+export interface LoginRequest {
+    phone: string;
+    password: string;
+  }
+  
+  export interface LoginResponse {
+    token: string;
+    user: {
+      id: string;
+      email: string;
+      name: string;
+    };
+  }
+
+  // Hàm đăng nhập sử dụng Axios
+export const postLogin = async (credentials: LoginRequest): Promise<LoginResponse> => {
+{
+      const response = await axiosClient.post<LoginResponse>(`${AUTH_API_URL}/Login`, credentials);
+      return response.data;
+    
+
+  };
+
+}
+
+export const postLogout = async (): Promise<void> => {
+  await axiosClient.post(`${AUTH_API_URL}/logout`);
+} ;
